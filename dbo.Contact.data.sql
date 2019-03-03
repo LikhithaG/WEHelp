2b47192635e2c30937e30c1c98ebe93f46b444a3
LikhithaG <32934275+LikhithaG@users.noreply.github.com>
2019-03-03 22:36:08 +0530

    Delete dbo.Contact.data.sql

--- dbo.Contact.data.sql
+++
@@ -1,4 +0,0 @@
-﻿SET IDENTITY_INSERT [dbo].[Contact] ON
-INSERT INTO [dbo].[Contact] ([ID], [Name], [Content], [Mail], [CreatedAt], [SentAt]) VALUES (1, N'Likhitha', N'I will like  to contact you.Please cosider the request.', N'likhitha,gaddi@ggktech.com', N'2019-02-12', N'2019-02-12')
-INSERT INTO [dbo].[Contact] ([ID], [Name], [Content], [Mail], [CreatedAt], [SentAt]) VALUES (8, N'Akshita', N'I will like  to contact you.Please cosider the request.', N'akshita.ravula@ggktech.com', N'2019-02-25', N'2019-02-25')
-SET IDENTITY_INSERT [dbo].[Contact] OFF
