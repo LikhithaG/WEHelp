82ce351f575266d280b9b94b644ec190218c6eac
LikhithaG <32934275+LikhithaG@users.noreply.github.com>
2019-03-03 22:37:15 +0530

    Delete dbo.ScholorshipTypes.data.sql

--- dbo.ScholorshipTypes.data.sql
+++
@@ -1,4 +0,0 @@
-﻿SET IDENTITY_INSERT [dbo].[ScholorshipTypes] ON
-INSERT INTO [dbo].[ScholorshipTypes] ([ID], [Type]) VALUES (1, N'Private')
-INSERT INTO [dbo].[ScholorshipTypes] ([ID], [Type]) VALUES (2, N'Government')
-SET IDENTITY_INSERT [dbo].[ScholorshipTypes] OFF
